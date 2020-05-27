745f5b5ed4386a54584d35da1cb29e201db11677
Salemar <fireshowel@gmail.com>
2020-05-27 12:38:44 +0300

    fixed codestyle

--- src/walker/Walk.java
+++ src/walker/Walk.java
@@ -1,42 +1,62 @@
 package walker;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 
 public class Walk {
-    public static void main(String[] args) {
-        try {
-            walk(args[0], args[1]);
-        } catch (Exception e) {
-
-        }
-    }
-
     private static String inputFile;
     private static String outputFile;
     private static String currentline;
-    static private BufferedReader reader;
-    static private OutputStreamWriter fileWriter;
+    private static BufferedReader reader;
+    private static OutputStreamWriter fileWriter;
+
+    public static void main(String[] args) {
+        try {
+            walk(args[0], args[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     private static void openInputFile() throws IOException {
-        reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile),StandardCharsets.UTF_8));
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8));
     }
 
-    private static void openOutputFile() throws IOException {
+    private static void openOutputFile() {
         File outFile = new File(outputFile);
-        if (!outFile.exists()) {
-            if (outFile.getParentFile() != null) {
-                outFile.getParentFile().mkdirs();
+        try {
+            if (!outFile.exists()) {
+                if (outFile.getParentFile() != null) {
+                    if (outFile.getParentFile().mkdirs()) {
+                        throw new IOException();
+                    }
+                }
+
+                if (outFile.createNewFile()) {
+                    throw new IOException();
+                }
             }
-            outFile.createNewFile();
+
+            fileWriter = new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        fileWriter=new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8);
     }
 
-    public static void walk(String input,String output) throws IOException {
-        inputFile=input;
-        outputFile=output;
+
+    private static void getHash() throws IOException {
+        while ((currentline = reader.readLine()) != null) {
+
+            fileWriter.write(String.format("%08x ", HashCalculator.hash32(currentline)) + currentline + System.lineSeparator());
+
+        }
+    }
+
+    public static void walk(String input, String output) throws IOException {
+        inputFile = input;
+        outputFile = output;
         try {
             openInputFile();
             openOutputFile();
@@ -53,13 +73,7 @@ public class Walk {
     }
 
 
-    private static void getHash() throws IOException {
-        while ((currentline = reader.readLine()) != null) {
-
-            fileWriter.write(String.format("%08x ", HashCalculator.hash32(currentline)) + currentline + System.lineSeparator());
-
-        }
-    }
 }
+
 
 
